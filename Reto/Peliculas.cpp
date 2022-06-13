4498e70da32b4c0bceb656de3f9920a30849cd3e
Cynthia Quijas <105666910+cynthiaQuijas@users.noreply.github.com>
2022-06-13 11:25:33 -0500

    Update Peliculas.cpp

--- Reto/Peliculas.cpp
+++ Reto/Peliculas.cpp
@@ -13,7 +13,7 @@ Peliculas::Peliculas(string _iD, string _titulo, int _duracion,string _genero, d
    
 
 
-// Métodos adicionales
+// Otros métodos
 
 void Peliculas::leerArchivo() {
     
@@ -29,49 +29,39 @@ void Peliculas::leerArchivo() {
     string word;
     
     
-    // Abrir archivo de entrada
     fin.open("Pelicula.csv",ios::in);
     
     int cantidad=0;
     
     while (getline(fin,line)){
-        
-        // Muestra linea
+
         cout<< cantidad<< line<< endl;
         
-        // Separa las palabras split
         stringstream s(line);
     
     
         while(getline(s,word, ',')){
-            // El while xtrae caracteres de s y los almacena en word hasta que encuentra el delimitador
             
             int iR=0;
             row[iR++]= word;
         }
         
         arrPtrPeliculas[cantidad] = new Pelicula(row[0], row[1], stoi(row[2]), row[3], stod(row[4]), stoi(row[5]));
-            
-        //Se muestra en consola la separacion
+
         cout << "iD" << row[0] << "\n";
         cout << "Titulo" << row[1] << "\n";
         cout << "Duracion" << row[2] << "\n";
         cout << "Genero" << row[3] << "\n";
         cout << "Calificacion" << row[4] << "\n";
         cout << "Oscares" << row[5] << "\n" ;
-        
-        //Desplegamos en consola la pelicula con el metodo str() de Peli
+
         cout << "Dentro del objeto:" << arrPtrPeliculas[cantidad]->str()<<endl;
-        
-        //incrementar el atributo cantidad para la siguiente pelicula
+
         cantidad= cantidad + 1;
     }
     
     fin.close();
-        
-        //Sale de ciclo cuando ya no existen mas lineas en el archivo
-        
-        //Desplegar todas las Peliculas leidas
+
         for(int iR=0; iR<cantidad;iR++){
             cout << iR << "*" << arrPtrPeliculas[iR]->str() << endl;
         }
@@ -99,7 +89,7 @@ void Peliculas::leerArchivo() {
         return cantidad;
     }
   
-    // Métodos adicionales
+    // Otros métodos
     void Peliculas::reporteTodasLasPeliculas()
     {
         double promedio, acum;
