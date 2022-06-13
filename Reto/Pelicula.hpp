886ce50f20b3d20eb02444424ff87ee5f4475d28
Cynthia Quijas <105666910+cynthiaQuijas@users.noreply.github.com>
2022-06-13 11:29:33 -0500

    Update Pelicula.hpp

--- Reto/Pelicula.hpp
+++ Reto/Pelicula.hpp
@@ -5,28 +5,33 @@
 #ifndef Pelicula_hpp
 #define Pelicula_hpp
 
+
 #include <stdio.h>
-#include <iostream>
-using namespace std;
 #include "Video.hpp"
 
-class Pelicula{
-private: 
+class Pelicula : public Video {
+
+private:
     int oscares;
-
+    
+    
 public:
-    // Métodos constructores
+    
+    // Métodos Constructores
     Pelicula();
-    Pelicula(string _iD, string _titulo, int _duracion, 
-    string _genero, double _calificacionPromedio);
+    Pelicula(string _iD, string _titulo, int _duracion,string _genero, double _calificacionPromedio, int _oscares);
 
-    // Métodos modificadores (sets)
+    // Métodos de acceso (sets)
     void setOscares(int _oscares);
-
-    // Métodos constructores (gets)
+   
+    // Métodos de acceso (get)
     int getOscares();
 
+    // Otros métodos
     string str();
+    
 };
 
-#endif /* Pelicula_hpp */ 
+
+
+#endif /* Pelicula_hpp */
