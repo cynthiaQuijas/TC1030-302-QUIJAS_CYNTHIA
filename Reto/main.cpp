1f07d2009abfc678eee9484a00f0a9687a75a873
Cynthia Quijas <105666910+cynthiaQuijas@users.noreply.github.com>
2022-06-14 22:12:36 -0500

    Update main.cpp

--- Reto/main.cpp
+++ Reto/main.cpp
@@ -1,7 +1,8 @@
-// Cynthia Cristal Quijas Flores
+//
+//
+//Cynthia Cristal Quijas Flores
 // A01655996
-// main.cpp
-
+//
 //
 //  main.cpp
 //  Avance 3 Objetivos - Lectura de archivos csv: 2 archivos para cargar la info de las Series
@@ -21,26 +22,24 @@
 //
 
 #include <iostream>
-#include "Peliculas.hpp"
-#include "Series.hpp"
+#include "Peliculas.h"
+#include "Series.h" 
 
 int menuPeliculas(){
     int iOpcion;
     
-  /*  cout <<
-    "\n *******  Peliculas *******" <<
+    cout <<
+    "\n ** Peliculas " <<
     "\n1. Leer Peliculas  desde Archivo" <<
     "\n2. Reporte de todas las peliculas" <<
-    "\n3. Reporte de peliculas que tienen cierta Calificacion" <<
+    "\n3. Reporte de peliculas que tienen cierta calificacion" <<
     "\n4. Reporte de peliculas de cierto genero" <<
-    "\n\n *******  Series *******" <<
     "\n5. Leer Series  desde Archivo" <<
     "\n6. Reporte de todas las Series" <<
     "\n7. Reporte de Series que tienen cierta Calificacion" <<
     "\n8. Reporte de Series de cierto genero" <<
     "\n9. Calcular calificación de todas las Series" <<
-    
-    "\n\n0. SALIR\nTeclea la opcion:"; */
+    "\n0. SALIR\nTeclea la opcion:";
     cin >> iOpcion;
     return iOpcion;
 }
@@ -51,18 +50,18 @@ int main() {
     // se manda llamar al constructor - default
     Peliculas peliculas{};
     // Polimorfismo - {} llama al constructor default Video
-   // Videos2 videos{};
-    Series series{};
-    //   Pelicula arrPeliculas[50], pelicula{};
-    
+    // Videos2 videos{};
+    Series series{};  
+    //   Pelicula arrPeliculas[50], pelicula{}
+  
     int iOpcion;
     string sGenero, sId;
-    double dCal;
- 
+    float dCal;
+    int c;
+    
     // Leer el archivo de peliculas y cargarlo en el arreglo de apuntadores
     // dentro de la clase Peliculas
     peliculas.leerArchivo();
-    series.leerArchivo();
     
     // 1o Inicializar la vcc antes del ciclo
     iOpcion = menuPeliculas();
@@ -71,18 +70,20 @@ int main() {
         switch (iOpcion) {
                 // ***** Peliculas
             case 1: // 1. Leer Peliculas  desde Archivo" <<
-                peliculas.leerArchivo();
+                c = peliculas.leerArchivo();
+                cout<<c<< endl;
+                peliculas.setCantidadPeliculas(c);
                 break;
             case 2:// 2. Reporte de todas las peliculas" <<
-                peliculas.reporteTodasLasPeliculas();
+              peliculas.reporteTodasLasPeliculas();
                 break;
             case 3:// 3. Reporte de todas las peliculas con cierta Calificacion" <<
-              //  cout << "Ingresa la calificacion:";
+                cout << "Ingresa la calificacion:";
                 cin >> dCal;
                 peliculas.reporteConCalificacion(dCal);
                 break;
             case 4: // 4. Reporte de todas las peliculas con cierto genero" <<
-              //  cout << "Ingresa el Genero:";
+                cout << "Ingresa el Genero:";
                 cin >> sGenero;
                 peliculas.reporteGenero(sGenero);
                 break;
@@ -93,22 +94,22 @@ int main() {
                 series.reporteTodasLasSeries();
                  break;
             case 7:  // 7. Reporte de Series que tienen cierta Calificacion
-              //  cout << "Ingresa la calificación:";
+              cout << "Ingresa la calificación:";
                 cin >> dCal;
                 series.reporteConCalificacion(dCal);
                  break;
             case 8:  // 8. Reporte de Series de cierto genero
-              //  cout << "Ingresa el Genero:";
+              cout << "Ingresa el Genero:";
                 cin >> sGenero;
                 series.reporteGenero(sGenero);
               //  videos.reporteVideos();
                  break;
             case 9:  // 9. Calcular calificación de todas las Series
                 series.calcularCalificacionSeries();
-                 break;
+                 break; 
             default:
                 cout << "Opcion Incorrecta\n";
-                break;
+                break; 
         }
         //3o Actualizar la vcc dentro del ciclo
         iOpcion = menuPeliculas();
